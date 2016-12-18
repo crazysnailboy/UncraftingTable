b62fed1b2bb98bea4ada7db1ee4bdc1cb61ad45c
Chris Porter <microsoft-live@crazysnailboy.net>
2016-12-18 10:55:45 +0000

    fixed free enchanted books bug, fixed server crash bug, implemented version checker, added more chests for "PorteManteau" achievement

--- src/main/java/org/jglrxavpok/mods/decraft/GuiUncraftingTable.java
+++ src/main/java/org/jglrxavpok/mods/decraft/GuiUncraftingTable.java
@@ -19,18 +19,15 @@ public class GuiUncraftingTable extends GuiContainer
 
     public ContainerUncraftingTable container;
     private String blockName;
-    private boolean inverted;
     private World worldObj;
     private EntityPlayer player;
 
-    public GuiUncraftingTable(InventoryPlayer playerInventory, World world, String blockName, boolean inverted)
+    public GuiUncraftingTable(InventoryPlayer playerInventory, World world, String blockName)
     {
-    	super(new ContainerUncraftingTable(playerInventory, world, inverted));
-        //super(new ContainerUncraftingTable(playerInventory, world, inverted, x, y, z));
+    	super(new ContainerUncraftingTable(playerInventory, world));
     	
         container = (ContainerUncraftingTable)inventorySlots;
         this.blockName = blockName;
-        this.inverted = inverted;
         this.worldObj = world;
         this.player = playerInventory.player;
     }
@@ -49,27 +46,25 @@ public class GuiUncraftingTable extends GuiContainer
         int xSize = this.xSize;
         int ySize = this.ySize;
         
-        if (!inverted)
-        {
-        	// fontRendererObj.drawString:
-        	// Args: string, x, y, color, dropShadow
-        	
-        	
-        	// render the block name at the top of the gui
-            fontRendererObj.drawString(blockName, xSize / 2 - fontRendererObj.getStringWidth(blockName) / 2 + 1, 5, 4210752);
-            
-            // write "inventory" above the player inventory
-            fontRendererObj.drawString(I18n.format("container.inventory"), 6, ySize - 96 + 2, 4210752);
+    	// fontRendererObj.drawString:
+    	// Args: string, x, y, color, dropShadow
+    	
+    	
+    	// render the block name at the top of the gui
+        fontRendererObj.drawString(blockName, xSize / 2 - fontRendererObj.getStringWidth(blockName) / 2 + 1, 5, 4210752);
+        
+        // write "inventory" above the player inventory
+        fontRendererObj.drawString(I18n.format("container.inventory"), 6, ySize - 96 + 2, 4210752);
 
-            // write "compute:" above the input slots
-            String compute = I18n.format("uncrafting.compute") + ":";
-            fontRendererObj.drawString(TextFormatting.DARK_GRAY + compute + TextFormatting.RESET, 24 - fontRendererObj.getStringWidth(compute) / 2 + 1, 22, 0);
-            fontRendererObj.drawString(TextFormatting.GRAY + compute + TextFormatting.RESET, 24 - fontRendererObj.getStringWidth(compute) / 2, 21, 0);
-            
-            // write the xp cost above the arrow
-            Color darkGreen = new Color(75, 245, 75);
-            fontRendererObj.drawString(TextFormatting.DARK_GRAY + "" + TextFormatting.UNDERLINE + "" + (ModConfiguration.standardLevel + container.uncraftingCost) + " levels" + TextFormatting.RESET, xSize / 2 - fontRendererObj.getStringWidth((ModConfiguration.standardLevel + container.uncraftingCost) + " levels") / 2 + 1, ySize - 126 - 10, 0);
-            fontRendererObj.drawString(TextFormatting.UNDERLINE + "" + (ModConfiguration.standardLevel + container.uncraftingCost) + " levels" + TextFormatting.RESET, xSize / 2 - fontRendererObj.getStringWidth((ModConfiguration.standardLevel + container.uncraftingCost) + " levels") / 2, ySize - 127 - 10, darkGreen.getRGB());
+        // write "compute:" above the input slots
+        String compute = I18n.format("uncrafting.compute") + ":";
+        fontRendererObj.drawString(TextFormatting.DARK_GRAY + compute + TextFormatting.RESET, 24 - fontRendererObj.getStringWidth(compute) / 2 + 1, 22, 0);
+        fontRendererObj.drawString(TextFormatting.GRAY + compute + TextFormatting.RESET, 24 - fontRendererObj.getStringWidth(compute) / 2, 21, 0);
+        
+        // write the xp cost above the arrow
+        Color darkGreen = new Color(75, 245, 75);
+        fontRendererObj.drawString(TextFormatting.DARK_GRAY + "" + TextFormatting.UNDERLINE + "" + (ModConfiguration.standardLevel + container.uncraftingCost) + " levels" + TextFormatting.RESET, xSize / 2 - fontRendererObj.getStringWidth((ModConfiguration.standardLevel + container.uncraftingCost) + " levels") / 2 + 1, ySize - 126 - 10, 0);
+        fontRendererObj.drawString(TextFormatting.UNDERLINE + "" + (ModConfiguration.standardLevel + container.uncraftingCost) + " levels" + TextFormatting.RESET, xSize / 2 - fontRendererObj.getStringWidth((ModConfiguration.standardLevel + container.uncraftingCost) + " levels") / 2, ySize - 127 - 10, darkGreen.getRGB());
 
 
 //            // draw the arrow with the cross through it
@@ -78,53 +73,23 @@ public class GuiUncraftingTable extends GuiContainer
 //            this.mc.renderEngine.bindTexture(new ResourceLocation("uncraftingTable:textures/gui/container/uncrafting_gui.png"));
 //            this.drawTexturedModalRect(75, 32, 176, 0, 28, 21);
 //            GL11.glPopMatrix();
-            
-            
-            String string = container.uncraftingStatusText;
-            if (string != null)
-            {
-                UncraftingStatus msgType = container.uncraftingStatus;
-                TextFormatting format = TextFormatting.GREEN;
-                TextFormatting shadowFormat = TextFormatting.DARK_GRAY;
-                if (msgType == ContainerUncraftingTable.UncraftingStatus.ERROR)
-                {
-                    format = TextFormatting.WHITE;
-                    shadowFormat = TextFormatting.DARK_RED;
-                }
-                fontRendererObj.drawString(shadowFormat + string + TextFormatting.RESET, 6 + 1, ySize - 95 + 2 - fontRendererObj.FONT_HEIGHT, 0);
-                fontRendererObj.drawString(format + string + TextFormatting.RESET, 6, ySize - 96 + 2 - fontRendererObj.FONT_HEIGHT, 0);
-            }
-        }
-        else
+        
+        
+        String string = container.uncraftingStatusText;
+        if (string != null)
         {
-            int height = 166 - 8;
-            
-            fontRendererObj.drawString(blockName, xSize / 2 - fontRendererObj.getStringWidth(blockName) / 2 + 1, height - 5, 4210752);
-            fontRendererObj.drawString(I18n.format("container.inventory"), 6, height - ySize - 96 + 2, 4210752);
-
-            Color darkGreen = new Color(75, 245, 75);
-            String string1 = "Calculs:";
-            
-            fontRendererObj.drawString(TextFormatting.DARK_GRAY + string1 + TextFormatting.RESET, 24 - fontRendererObj.getStringWidth(string1) / 2 + 1, height - 22, 0);
-            fontRendererObj.drawString(TextFormatting.GRAY + string1 + TextFormatting.RESET, 24 - fontRendererObj.getStringWidth(string1) / 2, height - 21, 0);
-            fontRendererObj.drawString(TextFormatting.DARK_GRAY + "" + TextFormatting.UNDERLINE + "" + (ModConfiguration.standardLevel + container.uncraftingCost) + " levels" + TextFormatting.RESET, xSize / 2 - fontRendererObj.getStringWidth((ModConfiguration.standardLevel + container.uncraftingCost) + " levels") / 2 + 1, height - (ySize - 126 - 10), 0);
-            fontRendererObj.drawString(TextFormatting.UNDERLINE + "" + (ModConfiguration.standardLevel + container.uncraftingCost) + " levels" + TextFormatting.RESET, xSize / 2 - fontRendererObj.getStringWidth((ModConfiguration.standardLevel + container.uncraftingCost) + " levels") / 2, height - (ySize - 127 - 10), darkGreen.getRGB());
-
-            String string = container.uncraftingStatusText;
-            if (string != null)
+            UncraftingStatus msgType = container.uncraftingStatus;
+            TextFormatting format = TextFormatting.GREEN;
+            TextFormatting shadowFormat = TextFormatting.DARK_GRAY;
+            if (msgType == ContainerUncraftingTable.UncraftingStatus.ERROR)
             {
-                UncraftingStatus msgType = container.uncraftingStatus;
-                TextFormatting format = TextFormatting.GREEN;
-                TextFormatting shadowFormat = TextFormatting.DARK_GRAY;
-                if (msgType == ContainerUncraftingTable.UncraftingStatus.ERROR)
-                {
-                    format = TextFormatting.WHITE;
-                    shadowFormat = TextFormatting.DARK_RED;
-                }
-                fontRendererObj.drawString(shadowFormat + string + TextFormatting.RESET, 6 + 1, height - (ySize - 95 + 2 - fontRendererObj.FONT_HEIGHT), 0);
-                fontRendererObj.drawString(format + string + TextFormatting.RESET, 6, height - (ySize - 96 + 2 - fontRendererObj.FONT_HEIGHT), 0);
+                format = TextFormatting.WHITE;
+                shadowFormat = TextFormatting.DARK_RED;
             }
+            fontRendererObj.drawString(shadowFormat + string + TextFormatting.RESET, 6 + 1, ySize - 95 + 2 - fontRendererObj.FONT_HEIGHT, 0);
+            fontRendererObj.drawString(format + string + TextFormatting.RESET, 6, ySize - 96 + 2 - fontRendererObj.FONT_HEIGHT, 0);
         }
+
         GL11.glEnable(GL11.GL_LIGHTING);
     }
 
@@ -134,11 +99,8 @@ public class GuiUncraftingTable extends GuiContainer
         GL11.glPushMatrix();
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 
-        if (this.inverted) {
-            this.mc.renderEngine.bindTexture(new ResourceLocation("uncraftingTable:textures/gui/container/uncrafting_gui_redstoned.png"));
-        } else {
-            this.mc.renderEngine.bindTexture(new ResourceLocation("uncraftingTable:textures/gui/container/uncrafting_gui.png"));
-        }
+        this.mc.renderEngine.bindTexture(new ResourceLocation("uncraftingTable:textures/gui/container/uncrafting_gui.png"));
+
         int k = this.width / 2 - this.xSize / 2;
         int l = this.height / 2 - this.ySize / 2;
         this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
