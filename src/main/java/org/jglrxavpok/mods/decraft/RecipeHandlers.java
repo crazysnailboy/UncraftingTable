db043095f32bf856888e17a06814dbc4e219e5ed
Chris Porter <microsoft-live@crazysnailboy.net>
2017-01-01 22:49:09 +0000

    refactored recipe lookups and recipe handlers

--- src/main/java/org/jglrxavpok/mods/decraft/RecipeHandlers.java
+++ src/main/java/org/jglrxavpok/mods/decraft/RecipeHandlers.java
@@ -1,6 +1,11 @@
 package org.jglrxavpok.mods.decraft;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Iterables;
+
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.ShapedRecipes;
@@ -16,28 +21,33 @@ import net.minecraftforge.oredict.ShapelessOreRecipe;
 public final class RecipeHandlers
 {
 	
+	/**
+	 * Abstract base class extended by the different types of recipe handler
+	 *
+	 */
 	public static abstract class RecipeHandler
 	{
-		private Class<? extends IRecipe> recipeType;
+//		private Class<? extends IRecipe> recipeType;
 
 		public RecipeHandler(Class<? extends IRecipe> recipe)
 		{
-			this.recipeType = recipe;
+//			this.recipeType = recipe;
 		}
 		
-		public Class<? extends IRecipe> getType()
-		{
-			return recipeType;
-		}
+//		public Class<? extends IRecipe> getType()
+//		{
+//			return recipeType;
+//		}
 		
-		/**
-		 * Returns the "crafting grid" depending on the given Recipe
-		 */
 		public abstract ItemStack[] getCraftingGrid(IRecipe s);
 	}
 	
-	
 
+	
+	/**
+	 * Handler for vanilla Minecraft shaped recipes
+	 *
+	 */
 	public static class ShapedRecipeHandler extends RecipeHandler
 	{
 		public ShapedRecipeHandler(Class<? extends IRecipe> recipe)
@@ -48,19 +58,16 @@ public final class RecipeHandlers
 		@Override
 		public ItemStack[] getCraftingGrid(IRecipe r)
 		{
+			// ShapedRecipes.recipeItems is already an ItemStack[], so just return that
 			return ((ShapedRecipes)r).recipeItems;
-			
-//			ItemStack[] stacks = new ItemStack[9];
-//			ShapedRecipes shaped = (ShapedRecipes)r;
-//			for (int j = 0;j<shaped.recipeItems.length;j++)
-//			{
-//				stacks[j] = shaped.recipeItems[j];
-//			}
-//			return stacks;
 		}
 	}
 	
 	
+	/**
+	 * Handler for vanilla Minecraft shapeless recipes
+	 *
+	 */
 	public static class ShapelessRecipeHandler extends RecipeHandler
 	{
 		public ShapelessRecipeHandler(Class<? extends IRecipe> recipe)
@@ -71,17 +78,16 @@ public final class RecipeHandlers
 		@Override
 		public ItemStack[] getCraftingGrid(IRecipe r)
 		{
-			ItemStack[] stacks = new ItemStack[9];
-			ShapelessRecipes shaped = (ShapelessRecipes)r;
-			for (int j = 0;j<shaped.recipeItems.size();j++)
-			{
-				stacks[j] = (ItemStack) shaped.recipeItems.get(j);
-			}
-			return stacks;
+			// ShapelessRecipes.recipeItems is a List<ItemStack>, so convert it to an ItemStack[] and return
+			return Iterables.toArray(Iterables.filter(((ShapelessRecipes)r).recipeItems, ItemStack.class), ItemStack.class);
 		}
 	}
 	
 	
+	/**
+	 * Handler for shaped recipes which utilise the Forge Ore Dictionary
+	 *
+	 */
 	public static class ShapedOreRecipeHandler extends RecipeHandler
 	{
 		public ShapedOreRecipeHandler(Class<? extends IRecipe> recipe)
@@ -92,28 +98,29 @@ public final class RecipeHandlers
 		@Override
 		public ItemStack[] getCraftingGrid(IRecipe r)
 		{
-			ItemStack[] stacks = new ItemStack[9];
-			ShapedOreRecipe shaped = (ShapedOreRecipe)r;
-			for (int j = 0;j<shaped.getInput().length;j++)
+			List<ItemStack> stacks = new ArrayList<ItemStack>();
+			
+			for ( Object target : ((ShapedOreRecipe)r).getInput())
 			{
-				if (shaped.getInput()[j] instanceof ItemStack)
+				if (target instanceof ItemStack)
 				{
-					stacks[j] = (ItemStack) shaped.getInput()[j];
+					stacks.add((ItemStack)target);
 				}
-				else if (shaped.getInput()[j] instanceof java.util.List)
+				else if (target instanceof List)
 				{
-					Object o = ((java.util.List)shaped.getInput()[j]).get(0);
-					if (o instanceof ItemStack)
-					{
-						stacks[j] = (ItemStack)o;
-					}
+					stacks.add(((List<ItemStack>)target).get(0));
 				}
 			}
-			return stacks;
+			
+			return stacks.toArray(new ItemStack[9]);
 		}
 	}
 
 	
+	/**
+	 * Handler for shapeless recipes which utilise the Forge Ore Dictionary
+	 *
+	 */
 	public static class ShapelessOreRecipeHandler extends RecipeHandler
 	{
 		public ShapelessOreRecipeHandler(Class<? extends IRecipe> recipe)
@@ -124,45 +131,109 @@ public final class RecipeHandlers
 		@Override
 		public ItemStack[] getCraftingGrid(IRecipe r)
 		{
-			ItemStack[] stacks = new ItemStack[9];
-			ShapelessOreRecipe shaped = (ShapelessOreRecipe)r;
-			for (int j = 0;j<shaped.getInput().size();j++)
+			List<ItemStack> stacks = new ArrayList<ItemStack>();
+			
+			for ( Object target : ((ShapelessOreRecipe)r).getInput())
 			{
-				if (shaped.getInput().get(j) instanceof ItemStack)
+				if (target instanceof ItemStack)
 				{
-					stacks[j] = (ItemStack) shaped.getInput().get(j);
+					stacks.add((ItemStack)target);
 				}
-				else if (shaped.getInput().get(j) instanceof java.util.List)
+				else if (target instanceof List)
 				{
-					Object o = ((java.util.List)shaped.getInput().get(j)).get(0);
-					if (o instanceof ItemStack)
-					{
-						stacks[j] = (ItemStack)o;
-					}
+					stacks.add(((List<ItemStack>)target).get(0));
 				}
 			}
-			return stacks;
+			
+			return stacks.toArray(new ItemStack[9]);
 		}
 	}
 	
 	
+	/**
+	 * Handler for shaped recipes from the Mekanism mod
+	 *
+	 */
+	public static class ShapedMekanismRecipeHandler extends RecipeHandler
+	{
+		public ShapedMekanismRecipeHandler(Class<? extends IRecipe> recipe) 
+		{
+			super(recipe);
+		}
+		
+		@Override
+		public ItemStack[] getCraftingGrid(IRecipe r)
+		{
+			try
+			{
+				List<ItemStack> stacks = new ArrayList<ItemStack>();
+				
+				for ( Object target : (Object[])Class.forName("mekanism.common.recipe.ShapedMekanismRecipe").getMethod("getInput", (Class[])null).invoke(r))
+				{
+					if (target instanceof ItemStack)
+					{
+						stacks.add((ItemStack)target);
+					}
+					else if (target instanceof ArrayList)
+					{
+						stacks.add(((ArrayList<ItemStack>)target).get(0));
+					}
+				}
+				
+				return stacks.toArray(new ItemStack[9]);
+			}
+			catch(Exception ex)
+			{
+				System.out.println("ShapedMekanismRecipeHandler.getCraftingGrid: " + ex.getMessage());
+				System.out.println(ex.getStackTrace());
+			}
+			return null;
+		}
+		
+	}
+	
+	
+	/**
+	 * Handler for shapeless recipes from the Mekanism mod
+	 *
+	 */
+	public static class ShapelessMekanismRecipeHandler extends RecipeHandler
+	{
+		public ShapelessMekanismRecipeHandler(Class<? extends IRecipe> recipe) 
+		{
+			super(recipe);
+		}
+		
+		@Override
+		public ItemStack[] getCraftingGrid(IRecipe r)
+		{
+			try
+			{
+				List<ItemStack> stacks = new ArrayList<ItemStack>();
+				
+				for ( Object target : (ArrayList<Object>)Class.forName("mekanism.common.recipe.ShapelessMekanismRecipe").getMethod("getInput", (Class[])null).invoke(r))
+				{
+					if (target instanceof ItemStack)
+					{
+						stacks.add((ItemStack)target);
+					}
+					else if (target instanceof ArrayList)
+					{
+						stacks.add(((ArrayList<ItemStack>)target).get(0));
+					}
+				}
+				
+				return stacks.toArray(new ItemStack[9]);
+			}
+			catch(Exception ex)
+			{
+				System.out.println("ShapelessMekanismRecipeHandler.getCraftingGrid: " + ex.getMessage());
+				System.out.println(ex.getStackTrace());
+			}
+			return null;
+		}
+		
+	}
+	
 
-//	/**
-//	 * Default Recipe Handlers
-//	 */
-//	public static final RecipeHandler DEFAULT_SHAPELESS_RECIPE_HANDLER = new ShapelessRecipeHandler(ShapelessRecipes.class);
-//	public static final RecipeHandler DEFAULT_SHAPED_RECIPE_HANDLER = new ShapedRecipeHandler(ShapedRecipes.class);
-//	public static final RecipeHandler DEFAULT_SHAPELESS_ORE_RECIPE_HANDLER = new ShapelessOreRecipeHandler(ShapelessOreRecipe.class);
-//	public static final RecipeHandler DEFAULT_SHAPED_ORE_RECIPE_HANDLER = new ShapedOreRecipeHandler(ShapedOreRecipe.class);
-//	
-//	/**
-//	 * Set the default Recipe Handlers
-//	 */
-//	public static void load()
-//	{
-//		UncraftingManager.setRecipeHandler(ShapelessRecipes.class, DEFAULT_SHAPELESS_RECIPE_HANDLER);
-//		UncraftingManager.setRecipeHandler(ShapedRecipes.class, DEFAULT_SHAPED_RECIPE_HANDLER);
-//		UncraftingManager.setRecipeHandler(ShapelessOreRecipe.class, DEFAULT_SHAPELESS_ORE_RECIPE_HANDLER);
-//		UncraftingManager.setRecipeHandler(ShapedOreRecipe.class, DEFAULT_SHAPED_ORE_RECIPE_HANDLER);
-//	}
 }
--- src/main/java/org/jglrxavpok/mods/decraft/UncraftingManager.java
+++ src/main/java/org/jglrxavpok/mods/decraft/UncraftingManager.java
@@ -1,23 +1,18 @@
 package org.jglrxavpok.mods.decraft;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.jglrxavpok.mods.decraft.RecipeHandlers.RecipeHandler;
+import org.jglrxavpok.mods.decraft.RecipeHandlers.ShapedMekanismRecipeHandler;
 import org.jglrxavpok.mods.decraft.RecipeHandlers.ShapedOreRecipeHandler;
 import org.jglrxavpok.mods.decraft.RecipeHandlers.ShapedRecipeHandler;
+import org.jglrxavpok.mods.decraft.RecipeHandlers.ShapelessMekanismRecipeHandler;
 import org.jglrxavpok.mods.decraft.RecipeHandlers.ShapelessOreRecipeHandler;
 import org.jglrxavpok.mods.decraft.RecipeHandlers.ShapelessRecipeHandler;
 import org.jglrxavpok.mods.decraft.common.config.ModConfiguration;
-import org.jglrxavpok.mods.decraft.item.uncrafting.mekanism.MekanismRecipeHandlers.ShapedMekanismRecipeHandler;
-import org.jglrxavpok.mods.decraft.item.uncrafting.mekanism.MekanismRecipeHandlers.ShapelessMekanismRecipeHandler;
 
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-
-import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
@@ -29,54 +24,12 @@ import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 /**
- * Main part of the Uncrafting Table. The manager is used to parse the existing recipes and find the correct one depending on the given stack.
  * @author jglrxavpok
+ *
  */
 public class UncraftingManager 
 {
 	
-//	private static List<IRecipe> vanillaRecipeList;
-//	
-//	private static List<IRecipe> reflectedRecipeList;
-	
-	
-//	private static List<IRecipe> getVanillaRecipeList()
-//	{
-//		List<IRecipe> recipeList = CraftingManager.getInstance().getRecipeList();
-//		
-//		List<IRecipe> vanillaRecipeList = new ArrayList<IRecipe>();
-//		vanillaRecipeList.addAll(Lists.newArrayList(Iterables.filter(recipeList, ShapedRecipes.class)));
-//		vanillaRecipeList.addAll(Lists.newArrayList(Iterables.filter(recipeList, ShapedOreRecipe.class)));
-//		vanillaRecipeList.addAll(Lists.newArrayList(Iterables.filter(recipeList, ShapelessRecipes.class)));
-//		vanillaRecipeList.addAll(Lists.newArrayList(Iterables.filter(recipeList, ShapelessOreRecipe.class)));
-//		
-//		return vanillaRecipeList;
-//	}
-//	
-//	private static List<IRecipe> getReflectedRecipeList()
-//	{
-//		List<IRecipe> recipeList = CraftingManager.getInstance().getRecipeList();
-//		List<IRecipe> reflectedRecipeList = new ArrayList<IRecipe>();
-//		
-//		String[] classNames = new String[] { 
-//			"mekanism.common.recipe.ShapedMekanismRecipe"
-//		};
-//		
-//		for ( String className : classNames )
-//		{
-//			try
-//			{
-//				Class c = Class.forName(className);
-//				reflectedRecipeList.addAll(Lists.newArrayList(Iterables.filter(recipeList, c)));
-//			}
-//			catch(Exception ex) { }
-//		}
-//		
-//		return reflectedRecipeList;
-//	}
-	
-	
-
 	private static Boolean canUncraftItem(ItemStack itemStack)
 	{
 		String uniqueIdentifier = Item.REGISTRY.getNameForObject(itemStack.getItem()).toString();
@@ -88,14 +41,11 @@ public class UncraftingManager
 	
 	public static List<Integer> getStackSizeNeeded(ItemStack item)
 	{
-//		System.out.println("\t" + "getStackSizeNeeded");
-//		System.out.println("\t" + item.getItem().getUnlocalizedName());
-//		System.out.println("\t" + item.getDisplayName());
-		
 		List<Integer> list = new ArrayList<Integer>();
 		if (!canUncraftItem(item)) return list;
 		
-		for ( IRecipe recipe : CraftingManager.getInstance().getRecipeList())
+		List<IRecipe> recipeList = CraftingManager.getInstance().getRecipeList(); 
+		for ( IRecipe recipe : recipeList )
 		{
 			ItemStack recipeOutput = recipe.getRecipeOutput();
 			if (recipeOutput != null)
@@ -120,19 +70,13 @@ public class UncraftingManager
 	}
 	
 	
-	
-	
 	public static List<ItemStack[]> getUncraftResults(ItemStack item)
 	{
-//		System.out.println("getUncraftResults");
-//		System.out.println(item.getItem().getUnlocalizedName());
-//		System.out.println(item.getDisplayName());
-//		System.out.println("isDamageable: " + item.getItem().isDamageable());
-
 		List<ItemStack[]> list = new ArrayList<ItemStack[]>();
 		if (!canUncraftItem(item)) return list;
 		
-		for ( IRecipe recipe : CraftingManager.getInstance().getRecipeList() )
+		List<IRecipe> recipeList = CraftingManager.getInstance().getRecipeList(); 
+		for ( IRecipe recipe : recipeList )
 		{
 			ItemStack recipeOutput = recipe.getRecipeOutput();
 			if (ItemStack.areItemsEqualIgnoreDurability(item, recipeOutput) && recipeOutput.stackSize <= item.stackSize)
@@ -165,9 +109,6 @@ public class UncraftingManager
 		
 		try
 		{
-//			Class c = Class.forName("mekanism.common.recipe.ShapedMekanismRecipe");
-//			if (c.isInstance(recipe)) return new ShapedMekanismRecipeHandler(c);
-
 			Class c;
 			
 			c = Class.forName("mekanism.common.recipe.ShapedMekanismRecipe");
@@ -175,27 +116,6 @@ public class UncraftingManager
 
 			c = Class.forName("mekanism.common.recipe.ShapelessMekanismRecipe");
 			if (c.isInstance(recipe)) return new ShapelessMekanismRecipeHandler(c);
-			
-			
-			
-//			HashMap<String, Class> reflectedHandlers = new HashMap<String, Class>();
-//			reflectedHandlers.put("mekanism.common.recipe.ShapedMekanismRecipe", ShapedMekanismRecipeHandler.class);
-//			reflectedHandlers.put("mekanism.common.recipe.ShapelessMekanismRecipe", ShapelessMekanismRecipeHandler.class);
-//			
-//			for ( String className : reflectedHandlers.keySet())
-//			{
-//				try
-//				{
-//					Class recipeClass = Class.forName(className);
-//					Class handlerClass = reflectedHandlers.get(className);
-//					
-//					if (recipeClass.isInstance(recipe))
-//					{
-//						return (RecipeHandler)(handlerClass.getConstructor(IRecipe.class).newInstance(recipeClass));
-//					}
-//				}
-//				catch(Exception ex) { }
-//			}
 			
 		}
 		catch(Exception ex) { 
@@ -207,8 +127,6 @@ public class UncraftingManager
 	
 	public static void postInit()
 	{
-//		vanillaRecipeList = getVanillaRecipeList();
-//		reflectedRecipeList = getReflectedRecipeList();
 	}
 	
 }
