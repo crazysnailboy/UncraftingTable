14d121d1e930bb0a023b2a02ba1724f8f58c181f
Chris Porter <microsoft-live@crazysnailboy.net>
2017-01-04 14:47:24 +0000

    implemented UncraftingResult class, moved calculation logic into UncraftingManager

---
+++ src/main/java/org/jglrxavpok/mods/decraft/item/uncrafting/UncraftingResult.java
@@ -0,0 +1,68 @@
+package org.jglrxavpok.mods.decraft.item.uncrafting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import net.minecraft.item.ItemStack;
+
+public class UncraftingResult 
+{
+    
+    public ResultType resultType;
+    public List<Integer> minStackSizes;
+    public List<ItemStack[]> craftingGrids;
+    public int experienceCost;
+    public ItemStack containerItems;
+    
+    public UncraftingResult()
+    {
+    	this.resultType = ResultType.INACTIVE;
+    	this.minStackSizes = new ArrayList<Integer>();
+    	this.craftingGrids = new ArrayList<ItemStack[]>();
+    	this.experienceCost = 0;
+    }
+    
+    
+
+//    public ResultType getResultType() 
+//    {
+//        return resultType;
+//    }
+
+//    public List<Integer> getMinStackSizes() 
+//    {
+//        return minStackSizes;
+//    }
+    
+    public int getMinStackSize()
+    {
+    	return (minStackSizes.size() > 0 ? Collections.min(minStackSizes) : 1);
+    }
+
+//    public List<ItemStack[]> getCraftingGrids() 
+//    {
+//        return craftingGrids;
+//    }
+    
+//    public int getExperienceCost() 
+//    {
+//        return experienceCost;
+//    }
+    
+//    public ItemStack getContainerItems()
+//    {
+//    	return containerItems;
+//    }
+    
+
+    public enum ResultType 
+    {
+    	INACTIVE, NOT_UNCRAFTABLE, NOT_ENOUGH_ITEMS, NOT_ENOUGH_XP, VALID;
+    	
+    	public static Boolean isError(ResultType value)
+    	{
+    		return (value == NOT_UNCRAFTABLE || value == ResultType.NOT_ENOUGH_ITEMS || value == ResultType.NOT_ENOUGH_XP);
+    	}
+    }
+
+}
