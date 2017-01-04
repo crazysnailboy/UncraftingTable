bfbb037b21afdcbfcb947c80bb4f196714339ced
Chris Porter <microsoft-live@crazysnailboy.net>
2017-01-04 14:15:00 +0000

    implemented UncraftingResult class, moved calculation logic into UncraftingManager

--- src/main/java/org/jglrxavpok/mods/decraft/UncraftingManager.java
+++ src/main/java/org/jglrxavpok/mods/decraft/UncraftingManager.java
@@ -1,7 +1,11 @@
 package org.jglrxavpok.mods.decraft;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
@@ -14,11 +18,17 @@ import org.jglrxavpok.mods.decraft.RecipeHandlers.ShapelessMekanismRecipeHandler
 import org.jglrxavpok.mods.decraft.RecipeHandlers.ShapelessOreRecipeHandler;
 import org.jglrxavpok.mods.decraft.RecipeHandlers.ShapelessRecipeHandler;
 import org.jglrxavpok.mods.decraft.common.config.ModConfiguration;
+import org.jglrxavpok.mods.decraft.item.uncrafting.UncraftingResult;
+import org.jglrxavpok.mods.decraft.item.uncrafting.UncraftingResult.ResultType;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
@@ -102,7 +112,7 @@ public class UncraftingManager
 	 * @param itemStack The ItemStack containing the target item
 	 * @return A collection of the ItemStack arrays representing the crafting recipe - one element per recipe found
 	 */
-	public static List<ItemStack[]> getUncraftResults(ItemStack itemStack)
+	public static List<ItemStack[]> findMatchingRecipes(ItemStack itemStack)
 	{
 		List<ItemStack[]> list = new ArrayList<ItemStack[]>();
 		if (isUncraftingDisabledForItem(itemStack)) return list;
@@ -206,6 +216,103 @@ public class UncraftingManager
 		
 		return null;
 	}
+	
+	
+	
+	public static UncraftingResult getUncraftingResult(EntityPlayer player, ItemStack itemStack)
+	{
+		
+		UncraftingResult uncraftingResult = new UncraftingResult();
+		
+        // get the minimum stack sizes needed to uncraft the input item
+		uncraftingResult.minStackSizes = getStackSizeNeeded(itemStack);
+        // get the crafting grids which could result in the input item
+		uncraftingResult.craftingGrids = findMatchingRecipes(itemStack);
+        // determine the xp cost for the uncrafting operation
+		uncraftingResult.experienceCost = getUncraftingXpCost(itemStack);
+		
+        // if the minimum stack size is greater than the number of items in the slot
+		if (uncraftingResult.minStackSizes.size() > 0 && itemStack.stackSize < Collections.min(uncraftingResult.minStackSizes))
+		{
+			// set the result type as "not enough items"
+			uncraftingResult.resultType = ResultType.NOT_ENOUGH_ITEMS;
+		}
+		// if no crafting recipe could be found
+		else if (uncraftingResult.craftingGrids.size() == 0)
+		{
+			// set the result type as "not uncraftable"
+			uncraftingResult.resultType = ResultType.NOT_UNCRAFTABLE;
+		}
+		// if the player is not in creative mode, and doesn't have enough XP levels 
+		else if (!player.capabilities.isCreativeMode && player.experienceLevel < uncraftingResult.experienceCost)
+		{
+			// set the result type as "not enough xp"
+			uncraftingResult.resultType = ResultType.NOT_ENOUGH_XP;
+		}
+		// otherwise, the uncrafting operation can be performed
+		else
+		{
+			uncraftingResult.resultType = ResultType.VALID;
+		}
+		
+		return uncraftingResult;
+	}
+	
+	
+	public static List<ItemStack> getItemEnchantments(ItemStack itemStack, ItemStack containerItems)
+	{
+        // initialise a list of itemstacks to hold enchanted books  
+        ArrayList<ItemStack> enchantedBooks = new ArrayList<ItemStack>();
+        
+        // if the item being uncrafted has enchantments, and the container itemstack contains books
+        if (itemStack.isItemEnchanted() && containerItems != null && containerItems.getItem() == Items.book)
+        {
+            // build a map of the enchantments on the item in the input stack
+            Map itemEnchantments = EnchantmentHelper.getEnchantments(itemStack);
+            
+            // if the item has more than one enchantment, and we have at least the same number of books as enchantments
+            // create an itemstack of enchanted books with a single enchantment per book
+            if (itemEnchantments.size() > 1 && itemEnchantments.size() <= containerItems.stackSize)
+            {
+            	// iterate through the enchantments in the map
+                Iterator<?> enchantmentIds = itemEnchantments.keySet().iterator();
+                while (enchantmentIds.hasNext())
+                {
+                    int enchantmentId = (Integer)enchantmentIds.next();
+                	// create a new map of enchantments which will be applied to this book
+                    Map<Integer, Integer> bookEnchantments = new LinkedHashMap<Integer, Integer>();
+                    // copy the current enchantment into the map
+                    bookEnchantments.put(enchantmentId, (Integer)itemEnchantments.get(enchantmentId));
+                	// create an itemstack containing an enchanted book
+                    ItemStack enchantedBook = new ItemStack(Items.enchanted_book, 1);
+                    // place the enchantment onto the book
+                    EnchantmentHelper.setEnchantments(bookEnchantments, enchantedBook);
+                    // add the book to the enchanted books collection
+                    enchantedBooks.add(enchantedBook);
+                    // clear the book enchantments map
+                    bookEnchantments.clear();
+                }
+            }
+            
+            // if there's a single enchantment, or fewer books than enchantments
+            // copy all of the enchantments from the item onto a single book
+            else
+            {
+            	// create an itemstack containing an enchanted book
+                ItemStack enchantedBook = new ItemStack(Items.enchanted_book, 1);
+                // copy all of the enchantments from the map onto the book
+                EnchantmentHelper.setEnchantments(itemEnchantments, enchantedBook);
+                // add the book to the enchanted books collection
+                enchantedBooks.add(enchantedBook);
+            }
+            
+        }
+        
+        // return the list of enchanted books
+        return enchantedBooks;
+	}
+	
+	
 	
 	
 	public static void postInit()
