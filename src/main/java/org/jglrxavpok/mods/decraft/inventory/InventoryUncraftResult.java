b0dd8dbd490ec0c5db75f7a9e3b344b90c5d027d
Chris Porter <microsoft-live@crazysnailboy.net>
2017-01-08 12:31:15 +0000

    replaced uncrafting loop with single operation

--- src/main/java/org/jglrxavpok/mods/decraft/inventory/InventoryUncraftResult.java
+++ src/main/java/org/jglrxavpok/mods/decraft/inventory/InventoryUncraftResult.java
@@ -7,30 +7,21 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 
+
 /**
- * 
  * @author jglrxavpok
  *
  */
 public class InventoryUncraftResult implements IInventory
 {
-
-    private final NonNullList<ItemStack> stackList;
-    
-    
-    public InventoryUncraftResult()
-    {
-    	int width = 3;
-    	int height = 3;
-        this.stackList = NonNullList.<ItemStack>withSize(width * height, ItemStack.EMPTY);
-    }
-    
+    private final NonNullList<ItemStack> stackResult = NonNullList.<ItemStack>withSize(9, ItemStack.EMPTY);
     
     /**
      * Returns the number of slots in the inventory.
      */
     @Override
-    public int getSizeInventory() {
+    public int getSizeInventory() 
+    {
         return 9;
     }
 
@@ -38,8 +29,9 @@ public class InventoryUncraftResult implements IInventory
      * Returns the stack in the given slot.
      */
     @Override
-    public ItemStack getStackInSlot(int index) {
-        return index >= this.getSizeInventory() ? ItemStack.EMPTY : (ItemStack)this.stackList.get(index);
+    public ItemStack getStackInSlot(int index) 
+    {
+        return index >= this.getSizeInventory() ? ItemStack.EMPTY : (ItemStack)this.stackResult.get(index);
     }
 
     /**
@@ -48,7 +40,7 @@ public class InventoryUncraftResult implements IInventory
     @Override
     public ItemStack decrStackSize(int index, int count)
     {
-        ItemStack itemstack = ItemStackHelper.getAndSplit(this.stackList, index, count);
+        ItemStack itemstack = ItemStackHelper.getAndSplit(this.stackResult, index, count);
         return itemstack;
     }
 
@@ -59,7 +51,7 @@ public class InventoryUncraftResult implements IInventory
     @Override
     public ItemStack removeStackFromSlot(int index)
     {
-        return ItemStackHelper.getAndRemove(this.stackList, index);
+        return ItemStackHelper.getAndRemove(this.stackResult, index);
     }
 
     /**
@@ -68,13 +60,103 @@ public class InventoryUncraftResult implements IInventory
     @Override
     public void setInventorySlotContents(int index, ItemStack stack)
     {
-        this.stackList.set(index, stack);
+        this.stackResult.set(index, stack);
     }
+    
+    /**
+     * Returns the name of the inventory
+     */
+	@Override
+	public String getName() 
+	{
+		return null;
+	}
+
+    /**
+     * Returns if the inventory is named
+     */
+	@Override
+	public boolean hasCustomName() 
+	{
+		return false;
+	}
+
+    /**
+     * Get the formatted ChatComponent that will be used for the sender's username in chat
+     */
+	@Override
+	public ITextComponent getDisplayName() 
+	{
+		return null;
+	}
+    
+	/**
+     * Returns the maximum stack size for a inventory slot.
+     */
+    @Override
+    public int getInventoryStackLimit() 
+    {
+        return 64;
+    }
+    
+    /**
+     * For tile entities, ensures the chunk containing the tile entity is saved to disk later - the game won't think it
+     * hasn't changed and skip it.
+     */
+    @Override
+    public void markDirty() 
+    {
+    }
+
+    /**
+     * Do not make give this method the name canInteractWith because it clashes with Container
+     */
+	@Override
+	public boolean isUsableByPlayer(EntityPlayer player) 
+	{
+        return true;
+	}
+    
+	@Override
+	public void openInventory(EntityPlayer player) 
+	{
+	}
+
+	@Override
+	public void closeInventory(EntityPlayer player) 
+	{
+	}
+	
+    /**
+     * Returns true if automation is allowed to insert the given stack (ignoring stack size) into the given slot.
+     */
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) 
+    {
+        return true;
+    }
+    
+	@Override
+	public int getField(int id) 
+	{
+		return 0;
+	}
+
+	@Override
+	public void setField(int id, int value) 
+	{
+	}
+
+	@Override
+	public int getFieldCount() 
+	{
+		return 0;
+	}
     
 	@Override
     public boolean isEmpty()
 	{
-        for (ItemStack itemstack : this.stackList)
+        for (ItemStack itemstack : this.stackResult)
         {
             if (!itemstack.isEmpty())
             {
@@ -84,87 +166,10 @@ public class InventoryUncraftResult implements IInventory
         return true;
 	}
     
-    
-	/**
-     * Returns the maximum stack size for a inventory slot.
-     */
-    @Override
-    public int getInventoryStackLimit() {
-        return 1;
-    }
-
-    /**
-     * For tile entities, ensures the chunk containing the tile entity is saved to disk later - the game won't think it
-     * hasn't changed and skip it.
-     */
-    @Override
-    public void markDirty() {
-    }
-
-    /**
-     * Do not make give this method the name canInteractWith because it clashes with Container
-     */
 	@Override
-	public boolean isUsableByPlayer(EntityPlayer player) {
-        return true;
-	}
-    
-	@Override
-	public void openInventory(EntityPlayer player) {
-	}
-
-	@Override
-	public void closeInventory(EntityPlayer player) {
-	}
-    
-    /**
-     * Returns true if automation is allowed to insert the given stack (ignoring stack size) into the given slot.
-     */
-    @Override
-    public boolean isItemValidForSlot(int index, ItemStack stack) {
-        return true;
-    }
-
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean hasCustomName() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public ITextComponent getDisplayName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getField(int id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setField(int id, int value) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getFieldCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
-		
+	public void clear() 
+	{
+		this.stackResult.clear();
 	}
 
 }
