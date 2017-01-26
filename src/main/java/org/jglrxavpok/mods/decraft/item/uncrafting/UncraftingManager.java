59946a8e67dca5311ec92541d94fe93df528a473
Chris Porter <microsoft-live@crazysnailboy.net>
2017-01-26 12:54:24 +0000

    fix for nuggets with metadata

--- src/main/java/org/jglrxavpok/mods/decraft/item/uncrafting/UncraftingManager.java
+++ src/main/java/org/jglrxavpok/mods/decraft/item/uncrafting/UncraftingManager.java
@@ -1,5 +1,6 @@
 package org.jglrxavpok.mods.decraft.item.uncrafting;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -33,6 +34,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
+
 
 /**
  * Main part of the Uncrafting Table. The manager is used to parse the existing recipes and find the correct one depending on the given stack.
@@ -207,7 +209,7 @@ public class UncraftingManager
 						}
 
 						// add the stack size and the crafting grid to the results list
-						Map.Entry<NonNullList<ItemStack>,Integer> pair = new java.util.AbstractMap.SimpleEntry<NonNullList<ItemStack>,Integer>(craftingGrid, minStackSize);
+						Map.Entry<NonNullList<ItemStack>,Integer> pair = new AbstractMap.SimpleEntry<NonNullList<ItemStack>,Integer>(craftingGrid, minStackSize);
 						list.add(pair);
 					}
 				}
@@ -272,11 +274,11 @@ public class UncraftingManager
 		// RecipesMapExtending extends ShapedRecipes, and causes a crash when attempting to uncraft a map
 		if (recipe instanceof RecipesMapExtending) return null;
 		// vanilla Minecraft recipe handlers
-		if (recipe instanceof ShapelessRecipes) return new ShapelessRecipeHandler(ShapelessRecipes.class);
-		if (recipe instanceof ShapedRecipes) return new ShapedRecipeHandler(ShapedRecipes.class);
+		if (recipe instanceof ShapelessRecipes) return new ShapelessRecipeHandler();
+		if (recipe instanceof ShapedRecipes) return new ShapedRecipeHandler();
 		// Forge Ore Dictionary recipe handlers
-		if (recipe instanceof ShapelessOreRecipe) return new ShapelessOreRecipeHandler(ShapelessOreRecipe.class);
-		if (recipe instanceof ShapedOreRecipe) return new ShapedOreRecipeHandler(ShapedOreRecipe.class);
+		if (recipe instanceof ShapelessOreRecipe) return new ShapelessOreRecipeHandler();
+		if (recipe instanceof ShapedOreRecipe) return new ShapedOreRecipeHandler();
 		
 		return null;
 	}
@@ -297,27 +299,41 @@ public class UncraftingManager
 		
 
 		// iterate through the itemstacks in the crafting recipe to determine the unique materials used, and the total number of each item
-		HashMap<String, Integer> materials = new HashMap<String, Integer>();
+		HashMap<String, Map.Entry<ItemStack,Integer>> materials = new HashMap<String, Map.Entry<ItemStack,Integer>>();
 		for ( ItemStack recipeStack : craftingGrid )
 		{
 			if (recipeStack != ItemStack.EMPTY)
 			{
+				// get the unique identifier string for the item in the current stack, and append a metadata value if appropriate
 				String key = Item.REGISTRY.getNameForObject(recipeStack.getItem()).toString();
-				materials.put(key, (materials.containsKey(key) ? materials.get(key) : 0) + recipeStack.getCount());
+				if (recipeStack.getItemDamage() != 0) key += "," + recipeStack.getItemDamage();
+				
+				// if the map already contains a stack for this item, increment the number of items in the map
+				if (materials.containsKey(key))
+				{
+					materials.get(key).setValue(materials.get(key).getValue() + recipeStack.getCount());
+				}
+				// if the map doesn't already contain a stack for this item, add the stack and the number of items in the stack
+				else
+				{
+					Map.Entry<ItemStack,Integer> value = new AbstractMap.SimpleEntry<ItemStack,Integer>(recipeStack.copy(), recipeStack.getCount());
+					value.getKey().setCount(1);
+					materials.put(key, value);
+				}
 			}
 		}
 		
 		// for each unique material in the crafting recipe...
 		for ( String key : materials.keySet())
 		{
-			// get an itemstack of the material from it's registry name (TODO: probably don't need to condense this to a string in the first place...)
-			ItemStack materialStack = new ItemStack(Item.REGISTRY.getObject(new ResourceLocation(key)));
+			// get the itemstack of the material from the materials map
+			ItemStack materialStack = materials.get(key).getKey();
 
 			// check the ore dictionary to see if this material has a matching nugget
 			ItemStack nuggetStack = getNuggetForOre(materialStack);
 
 			
-			int amount = materials.get(key);
+			int amount = materials.get(key).getValue();
 			
 			int itemCount = 0;
 			int nuggetCount = 0;
@@ -342,8 +358,8 @@ public class UncraftingManager
 			else
 			{
 				// calculate the total number of full items which most closely represent the percentage durability remaining on the item
-				// rounding up or down to the nearest item
-				itemCount = (int)Math.round(amount * (durabilityPercentage / (double)100));
+				// rounding down to the nearest item
+				itemCount = (int)Math.floor(amount * (durabilityPercentage / (double)100));
 			}
 			
 			// flip the item count to become items to remove instead of items to leave
